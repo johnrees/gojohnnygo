58fbe4d5fef337a4db27ad4c90ff4a5ef4686e9a
John Rees <john@bitsushi.com>
2015-01-21 17:07:13 +0000

    web

--- web.go
+++ web.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-  http.HandleFunc("/", hello)
-  fmt.Println("listening...")
-  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-  if err != nil {
-    panic(err)
-  }
+	http.HandleFunc("/", gojohnnygo)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "go johnny go!")
+func gojohnnygo(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "<h1>go johnny go!</h1>")
+	fmt.Fprintln(res, "<p>A very simple website served by golang. <a href='https://github.com/johnrees/gojohnnygo'>Github Source</b></p>")
 }
